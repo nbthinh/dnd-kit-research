14e281d461b4a6d610f64093b3992bc43473f4f9
nbthinh <nguyenbuuthinh036@gmail.com>
2025-07-09 19:40:06 +0700

    codẻ

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
-import {DndContext} from '@dnd-kit/core';
-import {SortableContext} from '@dnd-kit/sortable';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable';
+
+import {
+  DndContext, 
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+
 import './App.css'
 import { Droppable } from './components/Droppable';
 import { Draggable } from './components/Draggable';
 import React, {useState} from 'react';
-
+import { SortableItem } from './components/SortableItem';
 function App() {
-  const [items] = useState([1, 2, 3]);
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event) {
+    const {active, over} = event;
+    
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+        
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 
   return (
     <>
-      <DndContext>
-        <SortableContext items={items}>
-          {/* ... */}
+      <DndContext 
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext 
+          items={items}
+          strategy={verticalListSortingStrategy}
+        >
+          {items.map(id => <SortableItem key={id} id={id} />)}
         </SortableContext>
       </DndContext>
     </>
